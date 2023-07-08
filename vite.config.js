0a843bd4fb6e70c02b9764257ed5197ccbb1b4d0
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-07-08 15:19:10 -0700

    added browser router to main.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
 });
