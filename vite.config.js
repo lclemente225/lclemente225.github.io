ce9cf9dbc4d842ef028c1a6ba1b102fbab517118
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-07-08 15:11:59 -0700

    added more to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 });
