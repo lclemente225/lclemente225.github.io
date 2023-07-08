9a0d5d9b68a7e64d5d2e03bebdecd78da19f0b8b
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-07-08 15:00:48 -0700

    changed switch to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage.jsx";
 import FirstNewsletterEmail from './components/projects/emails/newsletter/newsletter-email1.jsx';
 import FirstTransactionalEmail from './components/projects/emails/transactional-emails/first-transactional-email.jsx';
@@ -16,7 +16,7 @@ function App() {
   return (
     <div>
       <Router>
-        <Switch>
+        <Routes>
           <Route exact path="/" element={<Homepage isHomePage={isHomePage} PageState={PageState} />}/>
           <Route path="/first-newsletter-sample/" element={<FirstNewsletterEmail isHomePage={isHomePage} PageState={PageState}/>}/>
           <Route path="/first-promotional-sample/" element={<FirstPromotionalEmail isHomePage={isHomePage} PageState={PageState}/>}/>
@@ -24,7 +24,7 @@ function App() {
           <Route path="/sample-kaiba-corp-landingPage/" element={<KaibaCorpLanding isHomePage={isHomePage} PageState={PageState}/>}/>
           <Route path="/sample-yugi-tutor-landingPage/" element={<YugiTutorBusiness isHomePage={isHomePage} PageState={PageState}/>}/>
           <Route path="/sample-apartment-landingPage/" element={<ApartmentLandingPage isHomePage={isHomePage} PageState={PageState}/>}/>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   )
