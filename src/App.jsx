5cc90216a368974ab124c84f61e8c47ffcf24bcb
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-07-08 14:56:42 -0700

    added 'Switch' to app.js for HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage.jsx";
 import FirstNewsletterEmail from './components/projects/emails/newsletter/newsletter-email1.jsx';
 import FirstTransactionalEmail from './components/projects/emails/transactional-emails/first-transactional-email.jsx';
