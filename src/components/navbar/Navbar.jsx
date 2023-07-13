4733e18ba06fa8e809b6bebce0829b3edc7572c4
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-07-13 00:02:03 -0700

    adjusted navbar spacing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,15 +9,21 @@ const Navbar = ({PageState}) => {
         {PageState ? 
         <>
           <li>
-            <a href="https://lawrenceclemente.wordpress.com/list-of-posts/">My Blog</a>
+            <a href="https://lawrenceclemente.wordpress.com/list-of-posts/"
+            id="navbar-home-blog">
+              My Blog
+            </a>
           </li>
           <li>
-            <a href="https://lawrenceclemente.wordpress.com/">About</a>
+            <a href="https://lawrenceclemente.wordpress.com/"
+            id="navbar-home-about">
+              About
+            </a>
           </li>
         </>
        : 
           <li>
-            <a href="/">
+            <a href="/" id="navbar-project-page">
               <i className="fa-solid fa-left-long arrow-icon" />   
               <span> Back to portfolio</span>
             </a>
