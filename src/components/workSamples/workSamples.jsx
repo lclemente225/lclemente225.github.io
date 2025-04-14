e1c24060e65f18262cc86f6c621ff1abc921d3a4
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2025-04-14 11:49:46 -0700

    added work samples folder

---
+++ src/components/workSamples/workSamples.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useParams } from 'react-router-dom';
+import json from'./summaryJSON.json';
+import Navbar from '../../navbar/Navbar';
+import "./workSamples.css"
+import { HomepageFooter } from '../footer';
+
+export const ProjectSummary = ({isHomePage, PageState, }) => {
+    const {projectType, id} = useParams();
+    const [jsonData, insertJson] = useState(json);
+    useEffect(() => isHomePage(true), [PageState]);
+    const idToNum = id && parseInt(id);
+    const summaryInfo = jsonData && jsonData[projectType][idToNum];
+
+    function parseArray(array){
+        return array.map((value, index) => {
+            return (
+            <span> {value} {index < array.length-1 && "|"} </span>
+            )
+        })
+    }
+    
+    return(
+        <>
+        <Navbar PageState={PageState}/>
+            <div className="desc-section">
+                <h1>{summaryInfo.title}</h1>
+                <img src={summaryInfo.imgSrc}></img>
+                <p>
+                    {summaryInfo.desc}
+                </p>
+                <Link to={summaryInfo.link} className="project-link">
+                View Live Demo
+                </Link>
+                <div>
+                    What Technologies were Used?
+                    {parseArray(summaryInfo.tech)}
+                </div>
+            </div>
+            <div className="homepage-footer" id="foot">
+                <HomepageFooter/>
+            </div>
+        </>
+    )
+}
